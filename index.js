36f673a0af9d3edee3c575624e3b58874063ec15
Jonah Avillanoza <j.avillanoza@ufl.edu>
2022-05-22 17:22:55 -0400

    added parseInt and changed var to sidesStr

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function roll() {
-    console.log('clicked')
+    var sidesStr = prompt ('How many sides should the dice have?')
+    var numOfSides = parseInt(sidesStr)
+    if (!numOfSides) return
+    var rollResult = Math.floor(Math.random() * numOfSides) + 1
+  alert ('You rolled a ' + rollResult)
 }
