9e913b0e0b31f9949b4dc9a6dbdb9b87ed124798
Jonah Avillanoza <j.avillanoza@ufl.edu>
2022-05-22 18:55:59 -0400

    Finished Assignment 2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function greet () {
+   var name= prompt ('What is your name?')
+   if (name) {
+    alert ('Hello, ' + name) 
+   } else {
+       return
+   }
+
+   var oldAge= prompt ('How old are you ?')
+   var ageOfPers = parseInt (oldAge)
+   if (!ageOfPers) return
+
+ var BirthThisYear= confirm ('Have you had a Birthday this year?')
+
+ if (BirthThisYear) {
+     alert (name + ', you must have been born in ' + (new Date().getFullYear() - ageOfPers))
+     console.log('subtract their age from the current year')
+ } else {
+    alert (name + ', you must have been born in ' + (new Date().getFullYear() - ageOfPers - 1 ))
+     console.log('subtract their age plus one from the current year.')
+
+ }
+
+
+}
