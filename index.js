2b0ad2fa31e8eb0acbdd211241229915b10f819c
Jonah Avillanoza <j.avillanoza@ufl.edu>
2022-05-22 17:44:34 -0400

    Moved Dice Roll Game into Different Folder to Start Actual Assignment

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function roll() {
-    var sidesStr = prompt ('How many sides should the dice have?')
-    var numOfSides = parseInt(sidesStr)
-    if (!numOfSides) return
-    var rollResult = Math.floor(Math.random() * numOfSides) + 1
-  alert ('You rolled a ' + rollResult)
-  var rollAgain = confirm ('roll again?')
-
-  if (rollAgain) {
-      return roll()
-  }
-}
+// Your code here
